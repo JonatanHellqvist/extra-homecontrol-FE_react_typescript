aed736cab3ab84f7b987ef957c3c315ef13f801f
JonatanHellqvist <143435050+JonatanHellqvist@users.noreply.github.com>
2024-10-23 20:43:24 +0200

    SensorData komponent
    Dht11 sensor +
    FanControl

--- src/components/Devices.tsx
+++ src/components/Devices.tsx
@@ -1,8 +1,11 @@
 import HueDeviceList from "./HueDeviceList";
+import Dht11 from "./Sensordata/Dht11";
 
 function Devices() {
 	return (
 		<div>
+			<h3>Latest reading</h3>
+			<Dht11/>
 			<h3>Devices</h3>
 			<HueDeviceList/>
 		</div>
